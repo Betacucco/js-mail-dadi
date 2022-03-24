241231290407d6b75b525aa460a2a9d659841077
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-24 18:16:59 +0100

    Commit con commenti e miglioramenti

--- dadi/main.js
+++ dadi/main.js
@@ -1,11 +1,14 @@
+//Chiediamo all'utente se vuole giocare
 let confermaDiGioco = confirm("Vuoi vedere se sei piu' fortunato del computer?");
 
+//Se gioca verrano generati due numeri casuali
 if(confermaDiGioco){
     let numeroCasualeGiocatore = Math.floor((Math.random() * 6) + 1);
-    console.log(numeroCasualeGiocatore);
+    console.log("Il numero del giocatore e':" + numeroCasualeGiocatore);
     let numeroCasualeComputer = Math.floor((Math.random() * 6) + 1);
-    console.log(numeroCasualeComputer);
+    console.log("Il numero del computer e':" + numeroCasualeComputer);
 
+    //In base ai numeri casuali vincera' l'utente o il computer, oppure pareggiano
     if(numeroCasualeGiocatore > numeroCasualeComputer){
         alert("Hai vinto!");
     } else if(numeroCasualeGiocatore < numeroCasualeComputer){
--- mail/main.js
+++ mail/main.js
@@ -1,23 +1,43 @@
+//Lista delle email che abbiamo
 let listaEmail = [ "matteo@gmail.com", "fabio@gmail.com", "pietro@gmail.com", "anna@gmail.com"];
 
+//Inserimento da tastiera della email
 const inserisciEmail = prompt("Inserisci la tua email");
 console.log(inserisciEmail);
 
-for(let i = 0; i < listaEmail.length; i++){
-    if(listaEmail[i] == inserisciEmail){
+//Variabile booleana per uscire dal ciclo for
+let controlloDiAccessoInserimento = false;
+
+//Scorriamo l'array per tutta la sua lunghezza e finche' la variabile booleana non e' true
+for(let i = 0; (i < listaEmail.length) && (!controlloDiAccessoInserimento); i++){
+    //Se l'email inserita e' uguale ad una email nella lista usciamo dal for
+    if(inserisciEmail == listaEmail[i]){
+        
         alert("Accesso consentito!");
-        break;
-    } else {
+        controlloDiAccessoInserimento = true;
+        
+        //Altrimenti se non e' nel for decidiamo se aggiungerla o meno 
+    } else if(i == listaEmail.length - 1){
+
         let confermaDiInserimentoMail = confirm("Vuoi inserire la tua email in lista?");
+
         if(confermaDiInserimentoMail){
             listaEmail.push(inserisciEmail);
-            alert("Email inserita!");
-            break;
+            alert("Email inserita. Accesso Consentito");
+            controlloDiAccessoInserimento = true;
+    
         } else {
-            alert("Grazie e Arrivederci!");
-            break;
+            alert("Accesso Negato!");
+             controlloDiAccessoInserimento = true;
+    
         }
+
     }
 }
 
+
+//Stampa in console della lista e del valore booleano
 console.log(listaEmail);
+console.log(controlloDiAccessoInserimento);
+
+
