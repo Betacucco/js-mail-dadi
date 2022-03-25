7318fb81c439e54c394d56fe847944c82d537fa8
MarcoPieretti <marcopieretti50@gmail.com>
2022-03-25 10:05:46 +0100

    Pulizia del codice

--- mail/main.js
+++ mail/main.js
@@ -9,32 +9,25 @@ console.log(inserisciEmail);
 let controlloDiAccessoInserimento = false;
 
 //Scorriamo l'array per tutta la sua lunghezza e finche' la variabile booleana non e' true
-for(let i = 0; (i < listaEmail.length) && (!controlloDiAccessoInserimento); i++){
+for(let i = 0; i < listaEmail.length; i++){
     //Se l'email inserita e' uguale ad una email nella lista usciamo dal for
-    if(inserisciEmail == listaEmail[i]){
-        
+    if(inserisciEmail == listaEmail[i]){      
         alert("Accesso consentito!");
         controlloDiAccessoInserimento = true;
-        
-        //Altrimenti se non e' nel for decidiamo se aggiungerla o meno 
-    } else if(i == listaEmail.length - 1){
+    }        
+}       
+//Altrimenti se non e' nel for decidiamo se aggiungerla o meno 
+if(!controlloDiAccessoInserimento){
 
-        let confermaDiInserimentoMail = confirm("Vuoi inserire la tua email in lista?");
+    let confermaDiInserimentoMail = confirm("Vuoi inserire la tua email in lista?");
 
-        if(confermaDiInserimentoMail){
-            listaEmail.push(inserisciEmail);
-            alert("Email inserita. Accesso Consentito");
-            controlloDiAccessoInserimento = true;
-    
-        } else {
-            alert("Accesso Negato!");
-             controlloDiAccessoInserimento = true;
-    
-        }
-
+    if(confermaDiInserimentoMail){
+        listaEmail.push(inserisciEmail);
+        alert("Email inserita.");
+    } else {
+        alert("Accesso Negato!");
     }
 }
-
 
 //Stampa in console della lista e del valore booleano
 console.log(listaEmail);
